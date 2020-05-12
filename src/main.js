a60e92733c655a35b240caacefb2e14197faa285
ydkd <1606354057@qq.com>
2020-05-12 13:48:15 +0800

    day01

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router.js'
 import './plugins/element.js'
-
+// 导入全局样式表
+import './assets/css/global.css'
 Vue.config.productionTip = false
-
+    // 导入 字体图片文件样式
+import './assets/font/iconfont.css'
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
